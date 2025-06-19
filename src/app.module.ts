60f93f76d3a295652de4deb0592c8041bba32ad4
akiosTerr <stereoblindstudio@gmail.com>
2025-06-19 20:11:31 -0300

    clean app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Video } from './mux/entities/video.entity';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
 import typeOrmConfig from './config/typeorm.config';
